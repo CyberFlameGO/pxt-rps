497bf85ee34242bd1dad55252171c79cebfa296d
CyberFlame <cyberflameu@gmail.com>
2020-10-28 14:37:45 +1300

    chore: update js, py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-def on_gesture_shake():
+def pickShape():
     global myHand
     myHand = randint(1, 3)
-    if myHand == 1:
+    if myHand == rockIndex:
         rockImage.show_image(0)
-    if myHand == 2:
+    if myHand == paperIndex:
         paperImage.show_image(0)
-    if myHand == 3:
+    if myHand == scissorsIndex:
         scissorsImage.show_image(0)
+
+def on_gesture_shake():
+    pickShape()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 myHand = 0
 scissorsImage: Image = None
 paperImage: Image = None
 rockImage: Image = None
+scissorsIndex = 0
+paperIndex = 0
+rockIndex = 0
+rockIndex = 1
+paperIndex = 2
+scissorsIndex = 3
 rockImage = images.create_image("""
     . . . . .
     . # # # .
