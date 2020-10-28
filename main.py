354337a1ea07b80d860390dc892ec9e0b593e3f1
CyberFlame <cyberflameu@gmail.com>
2020-10-28 14:35:23 +1300

    chore: push

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def on_gesture_shake():
-    pass
+    global myHand
+    myHand = randint(1, 3)
+    if myHand == 1:
+        rockImage.show_image(0)
+    if myHand == 2:
+        paperImage.show_image(0)
+    if myHand == 3:
+        scissorsImage.show_image(0)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+myHand = 0
+scissorsImage: Image = None
+paperImage: Image = None
+rockImage: Image = None
 rockImage = images.create_image("""
     . . . . .
     . # # # .
--- main.ts
+++ main.ts
@@ -1,19 +1,28 @@
-input.onGesture(Gesture.Shake, function () {
+function pickShape () {
     myHand = randint(1, 3)
-    if (myHand == 1) {
+    if (myHand == rockIndex) {
         rockImage.showImage(0)
     }
-    if (myHand == 2) {
+    if (myHand == paperIndex) {
         paperImage.showImage(0)
     }
-    if (myHand == 3) {
+    if (myHand == scissorsIndex) {
         scissorsImage.showImage(0)
     }
+}
+input.onGesture(Gesture.Shake, function () {
+    pickShape()
 })
 let myHand = 0
 let scissorsImage: Image = null
 let paperImage: Image = null
 let rockImage: Image = null
+let scissorsIndex = 0
+let paperIndex = 0
+let rockIndex = 0
+rockIndex = 1
+paperIndex = 2
+scissorsIndex = 3
 rockImage = images.createImage(`
     . . . . .
     . # # # .
